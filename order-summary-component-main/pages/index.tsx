5f20aef643e92589cbd7b6fc6abb57d2a3dd27b6
HangCcz <hangchen1128@gmail.com>
2022-01-29 16:11:42 -0500

    completed challenge and added desktop view

--- order-summary-component-main/pages/index.tsx
+++ order-summary-component-main/pages/index.tsx
@@ -2,13 +2,16 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-indigo-200">
+    <div
+      className="background-image flex min-h-screen flex-col items-center justify-center  bg-indigo-200 bg-repeat-x"
+      style={{ backgroundImage: `url('/pattern-background-desktop.svg')` }}
+    >
       <Head>
         <title>Order Summary Component Main</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-1 flex-col items-center justify-center px-5">
+      <main className="flex max-w-md flex-1 flex-col items-center justify-center px-5">
         <div className="">
           <img src="./illustration-hero.svg" className="rounded-t-2xl" />
         </div>
@@ -23,7 +26,7 @@ export default function Home() {
             on any devices anywhere you like!
           </p>
 
-          <div className="mx-5 mt-7 flex items-center rounded-xl bg-indigo-50 py-4 px-0">
+          <div className="mx-5 mt-7 flex items-center rounded-xl bg-indigo-50 py-4 above-mobile:mx-10 ">
             <img src="icon-music.svg" alt="music icon" className="ml-4" />
 
             <div className="ml-5 flex flex-col">
@@ -33,18 +36,18 @@ export default function Home() {
               </p>
             </div>
 
-            <p className="ml-16 text-sm font-semibold text-bright-blue underline decoration-wavy">
+            <p className="ml-16 text-sm font-semibold text-bright-blue underline decoration-wavy above-mobile:ml-14 above-425px:ml-20 above-450px:ml-24">
               Change
             </p>
           </div>
-          <div className="mx-5 mt-6 rounded-xl bg-bright-blue px-2 py-3  text-center shadow-md shadow-indigo-500/50  hover:bg-indigo-500">
+          <div className="mx-5 mt-6 rounded-xl bg-bright-blue px-2 py-3 text-center  shadow-md shadow-indigo-500/50 hover:bg-indigo-500  above-mobile:mx-10">
             <button className="font-semibold text-white">
               Process to Payment
             </button>
           </div>
 
-          <div className="mx-5 mt-3 mb-7 rounded-xl bg-inherit px-2 py-2 text-center ">
-            <button className="font-bold text-desaturated-blue">
+          <div className=" mx-5 mt-3 mb-7 rounded-xl bg-inherit px-2 py-2 text-center  above-mobile:mx-10">
+            <button className="font-bold text-desaturated-blue hover:text-indigo-800">
               Cancel Order
             </button>
           </div>
@@ -53,7 +56,7 @@ export default function Home() {
 
       <footer className="mx-4 flex h-24 w-full flex-col items-center justify-center">
         <a href="https://github.com/HangCcZ" className="px-5">
-          <p className="">Coded By Hang Chen</p>
+          <p className="underline decoration-wavy">Coded by Hang Chen</p>
         </a>
         <a
           className="flex items-center justify-center "
--- order-summary-component-main/tailwind.config.js
+++ order-summary-component-main/tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'desaturated-blue': '#7280a7',
         'dark-blue': '#1f2f56',
       },
+      screens: {
+        'above-mobile': '400px',
+        'above-425px': '425px',
+        'above-450px': '450px',
+      },
     },
   },
   plugins: [],
